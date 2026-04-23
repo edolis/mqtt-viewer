da4bc5f726f6d3f769fe266a5a34015a56194270
Emanuele Dolis <emanuele.dolis@gmail.com>
2026-04-23 14:44:02 +0200

    fixed the null errors at compilation
    Signed-off-by: Emanuele Dolis <emanuele.dolis@gmail.com>

--- frontend/wailsjs/go/app/App.d.ts
+++ frontend/wailsjs/go/app/App.d.ts
@@ -32,7 +32,7 @@ export function DisconnectMqtt(arg1:number):Promise<void>;
 
 export function GetAllConnections():Promise<app.Connections>;
 
-export function GetAllSubscriptionsByConnectionId():Promise<{[key: number]: Array<models.Subscription>}>;
+export function GetAllSubscriptionsByConnectionId():Promise<Record<number, Array<models.Subscription>>>;
 
 export function GetEnvInfo():Promise<app.EnvInfo>;
 
